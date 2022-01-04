ba69c9b695f5a70ff02ad59dfad118efdbb78e68
danadarvishzadeh <danadarvishzadeh25@gmail.com>
2022-01-04 12:53:54 +0330

    readme

--- README.mk
+++ README.mk
@@ -1,5 +1,6 @@
 Changes until now:
 ==================
+
 1. Restructuring BLueprints.
 2. Restructuring models folder.
 3. Putting together the error classes and registering them with the app instance.
@@ -8,6 +9,7 @@ Changes until now:
 
 Ongoing changes:
 ==================
+
 - Full covering unittests
 - Open-api and Swager
 - SQl and Postgresql
